d0e6d8c234615f01ca92f3893863cbf0fe8262e3
AllaA_d2w3vtn <AllaA_d2w3vtn@LAPTOP-MFO7HJ67>
2024-01-23 11:06:52 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+
+    std::cout << "Чудная картина,\n";
+    std::cout << "Как ты мне родна:\n";
+    std::cout << "Белая равнина\n";
+    std::cout << "Полная луна\n";
+    // А.А.ФЕТ
+}
